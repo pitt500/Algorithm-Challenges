afda3cc020dc0bc77e4b4e473712bd5a73641720
pitt500 <pedro.rojaslo50@gmail.com>
2020-08-12 22:55:36 -0700

    Fix comments

--- LeetCode/Medium/528. Random Pick with Weight.playground/Contents.swift
+++ LeetCode/Medium/528. Random Pick with Weight.playground/Contents.swift
@@ -1,8 +1,11 @@
 import UIKit
 /*
- Given an array w of positive integers, where w[i] describes the weight of index i(0-indexed), write a function pickIndex which randomly picks an index in proportion to its weight.
+ Given an array w of positive integers, where w[i] describes the weight of index i(0-indexed),
+ write a function pickIndex which randomly picks an index in proportion to its weight.
 
- For example, given an input list of values w = [2, 8], when we pick up a number out of it, the chance is that 8 times out of 10 we should pick the number 1 as the answer since it's the second element of the array (w[1] = 8).
+ For example, given an input list of values w = [2, 8], when we pick up a number out of it,
+ the chance is that 8 times out of 10 we should pick the number 1 as the answer since it's
+ the second element of the array (w[1] = 8).
 
  */
 
